4ce310c4228ea298778ab1c734062d6bce8c0172
artuenric <artuenrick@gmail.com>
2023-08-22 17:35:23 -0300

    Reunir dados das chaves

--- customize/qkd-b92-scalabilty/plot.py
+++ customize/qkd-b92-scalabilty/plot.py
@@ -1,18 +1,45 @@
-import os
-
-def get_local_path():
+def comparacao(gerada, recebida):
     """
-    Obtém o caminho absoluto do diretório do código
+    Compara as duas listas (chaves geradas e recebidas) e retorna a quantidade de acertos do sniffer.
+
+    Args:
+        gerada (lista): Lista com a chave gerada.
+        recebida (lista): Lista com a chave recebida.
+    """
     
-    returns: 
-        current_dir (str): Caminho do código no computador.
-    """
+    sniffer_acertou = 0
+    for bit_gerado, bit_recebido in zip(gerada, recebida):
+        if bit_recebido != bit_gerado:
+            sniff_acertou += 1    
+    return sniffer_acertou
 
-    current_dir = os.path.abspath(os.path.dirname(__file__))
-    return current_dir
 
 
-def read_log(txt):
-    path = get_local_path()
-    #with open(f'{path}/log.txt', 'w', encoding='utf-8') as snff:
+
+
+
+
+
+def achar_gerada(txt, indice):
+    with open(txt, 'r') as f:
+        conteudo = f.readlines()
+    chave = str(chave)
+    for linha in conteudo:
+        if f"{indice} Chave gerada:" in linha:
+            chave = linha.split(':')[1]
         
+    
+"""def check(txt, execs):
+    contador = 1
+        
+    while contador <= execs:
+        for linha in conteudo:
+            if contador in linha:"""
+                
+
+conteudo =  ["[3] Chave gerada:[1, 0, 1, 1, 1, 0, 0, 0, 0, 1]"]
+indice = 3
+for linha in conteudo:
+    if f"[{indice}] Chave gerada:" in linha:
+        chave = linha.split(':')
+        print(chave[1])
